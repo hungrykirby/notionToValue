aecd848fc891df648dcc8604c631db20eb7a944f
hungrykirby <katsuki8hanawa6@gmail.com>
2020-02-26 23:48:47 +0900

    画像を複数保存できるように

--- app.py
+++ app.py
@@ -2,24 +2,28 @@ import csv
 import notionImporter
 import graphGenerater
 import datetime
+import time
 
 def main():
   notion = notionImporter.NotionCsvImporter('Untitled.csv')
   graph = graphGenerater.GraphGenerator('motion', None)
+  graph_filled = graphGenerater.GraphGenerator('motion filled', None)
   results = notion.convert_csv_to_list()
-  graph_data = {'x': [], 'y': []}
+  graph_data = {'x': [], 'y': [], 'timestamp': []}
   for i in range(len(results)):
     if results[i][2] == -1:
       continue
     graph_data['x'].append(results[i][1])
     graph_data['y'].append(results[i][2])
+    graph_data['timestamp'].append(float(results[i][1].timestamp()))
   if results == False:
     return False
-  for r in results:
-    print(r)
   # r_d_max = max(results, lambda r: r)
-  graph.set_max_min(max(graph_data['x']), 5, min(graph_data['x']), 0)
-  graph.create_motion_graph(graph_data['x'], graph_data['y'], True)
+  graph.set_max_min(max(graph_data['x']), 5, min(graph_data['x']), 1)
+  graph.create_motion_graph(graph_data['x'], graph_data['y'], '1')
+  tmp = graph_filled.fill_gap(graph_data['timestamp'], graph_data['y'], 10000)
+  graph_filled.set_max_min((max(graph_data['timestamp'])), 5, min(graph_data['timestamp']), 1)
+  graph_filled.create_motion_graph(tmp['x'], tmp['y'], '2')
   return True
 
 if __name__ == "__main__":
--- graphGenerater.py
+++ graphGenerater.py
@@ -6,7 +6,6 @@ import os
 class GraphGenerator:
   label_x = "x"
   label_y = "y"
-  fig = None
   title = None
   file_name = None
 
@@ -37,14 +36,40 @@ class GraphGenerator:
     self.label_x = label_x
     self.label_y = label_y
 
-  def create_motion_graph(self, x, y, save_flag = False):
+  def fill_gap(self, x, y, interval = 1):
+    len_list = len(x)
+    x_min = x[0]
+    x_max = x[len_list - 1]
+    i = x_min
+    xs = []
+    ys = []
+    while i < x_max:
+      target_x = i
+      tmp = np.asarray(x) - target_x
+      tmp2 = np.where(tmp <= 0)
+      idx = np.abs(tmp2).argmax()
+      i = i + interval
+      if idx > len_list - 1:
+        continue
+      x1 = x[idx]
+      x2 = x[idx + 1]
+      y1 = y[idx]
+      y2 = y[idx + 1]
+      # print(x_min, i, x_max)
+      target_y = (y2 - y1)*(target_x - x1)/(x2 - x1) + y1
+      xs.append(target_x)
+      ys.append(target_y)
+    return {'x': xs, 'y': ys}
+
+  def create_motion_graph(self, x, y, save_str = False):
     plt.title(self.title)
     plt.xlabel(self.label_x)
     plt.ylabel(self.label_y)
     plt.ylim(self.area['min']['y'], self.area['max']['y'])
     plt.plot(x, y)
-    if save_flag:
+    if save_str:
       rootdir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-      plt.savefig(join(os.getcwd(), 'imgs', 'saved.png'))
+      plt.savefig(join(os.getcwd(), 'imgs', 'saved' + save_str + '.png'))
+      plt.close()
     else:
       plt.show()
